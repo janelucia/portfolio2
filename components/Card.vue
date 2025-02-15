141171b9cd20dcd4db75f1f63d5468df72371d59
jane <janeluciaschoenfeld@gmail.com>
2025-02-15 21:30:08 +0100

    feat: add uni projects

--- components/Card.vue
+++ components/Card.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="rounded shadow-sm flex flex-col gap-2 md:flex-row" :class="bgCardColor">
-    <div v-if="image || icon" class="w-full md:w-1/3 flex justify-center items-center">
-      <NuxtPicture v-if="image" format="webp" :src="image" :img-attrs="{ class:'w-full rounded-t md:rounded-tr-none md:rounded-bl md:rounded-tl md:h-full' }"/>
-      <Icon v-if="icon" :name="icon" class="w-1/2 h-1/2 md:w-2/3 md:h-2/3 lg:w-full lg:h-full" />
+    <div v-if="image || icon" class="md:w-1/3 flex justify-center items-center overflow-hidden">
+      <NuxtPicture
+          v-if="image"
+          format="webp"
+          :src="image"
+          :img-attrs="{
+          class: 'w-full h-full object-cover rounded-t md:rounded-tr-none md:rounded-bl md:rounded-tl'
+        }"
+      />
+      <Icon
+          v-if="icon"
+          :name="icon"
+          class="w-1/2 h-1/2 md:w-2/3 md:h-2/3 lg:w-full lg:h-full"
+      />
       <div v-if="closeIcon" class="lg:hidden">
-        <Icon name="eva:close-fill" @click.stop="closeIcon" class="absolute top-2 right-2 hover:text-primary rounded-full w-8 h-8"/>
+        <Icon
+            name="eva:close-fill"
+            @click.stop="closeIcon"
+            class="absolute top-2 right-2 hover:text-primary rounded-full w-8 h-8"
+        />
       </div>
     </div>
     <div class="p-4 flex flex-col gap-2 flex-1 justify-evenly">
